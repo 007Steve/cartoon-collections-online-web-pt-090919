6d5117b321a82a424416028d3fa17822daea312b
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-21 03:56:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,13 @@
+ planeteer_calls =["earth","wind","fire","water","heart"]
   dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(array)
   index = 0
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls) #code an argument here
+  
+  
 end
 
 def long_planeteer_calls# code an argument here
