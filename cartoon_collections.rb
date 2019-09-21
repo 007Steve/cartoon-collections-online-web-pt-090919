bada6f78a2b45e2e2930fff0422c91bec1bfbd77
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-21 03:57:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(array)
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet(planeteer_calls) #code an argument here
-  
+def summon_captain_planet(array) #code an argument here
+   planeteer_calls = []
   
 end
 
