88987a412f665187688b1c255556986f402a5402
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-21 04:13:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,15 @@ planeteer_calls
 end
 
 def long_planeteer_calls(array)
-  array.any? do |word|
-    word.length > 4
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
-end
+
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
