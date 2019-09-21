f6bbed36bccbf29e328b95c90c012a7dce587734
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-09-21 04:14:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,6 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |cheese| |cheese = "cheddar"| |cheese ="gouda"|
-    |cheese ="camembert"|
+  
 end
 end
